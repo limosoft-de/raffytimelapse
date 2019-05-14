fa0ff5864b58559f3fa901c2f45778f69d55f1d2
limosoft <limosoft@users.sourceforge.net>
2019-05-14 22:23:17 +0200

    Release Candidate - Removed nUpdate references

--- Simple Time Lapse/form_Credits.Designer.vb
+++ Simple Time Lapse/form_Credits.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class form_Credits
     Inherits System.Windows.Forms.Form
 
     'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class form_Credits
     'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
     'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(form_Credits))
         Me.lbl_icons_head = New System.Windows.Forms.Label()
@@ -29,8 +29,6 @@ Partial Class form_Credits
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.lbl_ffmpeg_desc = New System.Windows.Forms.Label()
         Me.lbl_ffmpeg_head = New System.Windows.Forms.Label()
-        Me.lbl_nupdate_desc = New System.Windows.Forms.Label()
-        Me.lbl_nupdate_head = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -73,7 +71,7 @@ Partial Class form_Credits
         Me.Panel1.BackColor = System.Drawing.Color.Gray
         Me.Panel1.Controls.Add(Me.btn_ok)
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.Panel1.Location = New System.Drawing.Point(0, 339)
+        Me.Panel1.Location = New System.Drawing.Point(0, 238)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(313, 36)
         Me.Panel1.TabIndex = 4
@@ -101,36 +99,12 @@ Partial Class form_Credits
         Me.lbl_ffmpeg_head.TabIndex = 6
         Me.lbl_ffmpeg_head.Text = "FFmpeg"
         '
-        'lbl_nupdate_desc
-        '
-        Me.lbl_nupdate_desc.BackColor = System.Drawing.Color.Gray
-        Me.lbl_nupdate_desc.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.lbl_nupdate_desc.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lbl_nupdate_desc.Location = New System.Drawing.Point(10, 260)
-        Me.lbl_nupdate_desc.Name = "lbl_nupdate_desc"
-        Me.lbl_nupdate_desc.Size = New System.Drawing.Size(290, 60)
-        Me.lbl_nupdate_desc.TabIndex = 8
-        Me.lbl_nupdate_desc.Text = "Dominic Beger" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "nUpdate" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "www.nupdate.net"
-        '
-        'lbl_nupdate_head
-        '
-        Me.lbl_nupdate_head.AutoSize = True
-        Me.lbl_nupdate_head.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lbl_nupdate_head.ForeColor = System.Drawing.Color.White
-        Me.lbl_nupdate_head.Location = New System.Drawing.Point(9, 230)
-        Me.lbl_nupdate_head.Name = "lbl_nupdate_head"
-        Me.lbl_nupdate_head.Size = New System.Drawing.Size(69, 21)
-        Me.lbl_nupdate_head.TabIndex = 7
-        Me.lbl_nupdate_head.Text = "nUpdate"
-        '
         'form_Credits
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(313, 375)
-        Me.Controls.Add(Me.lbl_nupdate_desc)
-        Me.Controls.Add(Me.lbl_nupdate_head)
+        Me.ClientSize = New System.Drawing.Size(313, 274)
         Me.Controls.Add(Me.lbl_ffmpeg_head)
         Me.Controls.Add(Me.lbl_ffmpeg_desc)
         Me.Controls.Add(Me.Panel1)
@@ -155,6 +129,4 @@ Partial Class form_Credits
     Friend WithEvents Panel1 As Panel
     Friend WithEvents lbl_ffmpeg_desc As Label
     Friend WithEvents lbl_ffmpeg_head As Label
-    Friend WithEvents lbl_nupdate_desc As Label
-    Friend WithEvents lbl_nupdate_head As Label
 End Class
