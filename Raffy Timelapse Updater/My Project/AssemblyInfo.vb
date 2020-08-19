a13635c44b18853906f013016d1bcb414a758fed
limosoft <limosoft@users.sourceforge.net>
2020-08-19 11:23:00 +0200

    Updated Updater Version to 1.1.0

--- Raffy Timelapse Updater/My Project/AssemblyInfo.vb
+++ Raffy Timelapse Updater/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' indem Sie "*" wie unten gezeigt eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("1.1.0.0")>
+<Assembly: AssemblyFileVersion("1.1.0.0")>
 <Assembly: NeutralResourcesLanguage("en-US")>
