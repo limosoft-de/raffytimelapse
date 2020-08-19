15a6d4babf5a9ae62fff845dbc3e83dc1c124e0c
limosoft <limosoft@users.sourceforge.net>
2020-08-19 11:09:42 +0200

    Raffy Updater using XML-Configuration file

--- Raffy Timelapse Updater/form_main.Designer.vb
+++ Raffy Timelapse Updater/form_main.Designer.vb
@@ -36,45 +36,49 @@ Partial Class form_main
         'Picb_Icon
         '
         Me.Picb_Icon.Image = CType(resources.GetObject("Picb_Icon.Image"), System.Drawing.Image)
-        Me.Picb_Icon.Location = New System.Drawing.Point(12, 32)
+        Me.Picb_Icon.Location = New System.Drawing.Point(18, 48)
+        Me.Picb_Icon.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Picb_Icon.Name = "Picb_Icon"
-        Me.Picb_Icon.Size = New System.Drawing.Size(560, 100)
+        Me.Picb_Icon.Size = New System.Drawing.Size(840, 150)
         Me.Picb_Icon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.Picb_Icon.TabIndex = 0
         Me.Picb_Icon.TabStop = False
         '
         'Pb_Progress
         '
-        Me.Pb_Progress.Location = New System.Drawing.Point(12, 203)
+        Me.Pb_Progress.Location = New System.Drawing.Point(18, 304)
+        Me.Pb_Progress.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Pb_Progress.Name = "Pb_Progress"
-        Me.Pb_Progress.Size = New System.Drawing.Size(560, 23)
+        Me.Pb_Progress.Size = New System.Drawing.Size(840, 34)
         Me.Pb_Progress.TabIndex = 1
         '
         'Lbl_Title
         '
         Me.Lbl_Title.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Lbl_Title.Location = New System.Drawing.Point(12, 142)
+        Me.Lbl_Title.Location = New System.Drawing.Point(18, 213)
+        Me.Lbl_Title.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Lbl_Title.Name = "Lbl_Title"
-        Me.Lbl_Title.Size = New System.Drawing.Size(560, 25)
+        Me.Lbl_Title.Size = New System.Drawing.Size(840, 38)
         Me.Lbl_Title.TabIndex = 2
         Me.Lbl_Title.Text = "Raffy Timelapse Updater"
         Me.Lbl_Title.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'Lbl_Status
         '
-        Me.Lbl_Status.AutoSize = True
-        Me.Lbl_Status.Location = New System.Drawing.Point(9, 229)
+        Me.Lbl_Status.Location = New System.Drawing.Point(14, 344)
+        Me.Lbl_Status.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Lbl_Status.Name = "Lbl_Status"
-        Me.Lbl_Status.Size = New System.Drawing.Size(169, 13)
+        Me.Lbl_Status.Size = New System.Drawing.Size(844, 34)
         Me.Lbl_Status.TabIndex = 3
         Me.Lbl_Status.Text = "Checking internet connection..."
         '
         'Lbl_RaffyVersion
         '
         Me.Lbl_RaffyVersion.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Lbl_RaffyVersion.Location = New System.Drawing.Point(9, 169)
+        Me.Lbl_RaffyVersion.Location = New System.Drawing.Point(14, 254)
+        Me.Lbl_RaffyVersion.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Lbl_RaffyVersion.Name = "Lbl_RaffyVersion"
-        Me.Lbl_RaffyVersion.Size = New System.Drawing.Size(560, 25)
+        Me.Lbl_RaffyVersion.Size = New System.Drawing.Size(840, 38)
         Me.Lbl_RaffyVersion.TabIndex = 4
         Me.Lbl_RaffyVersion.Text = "Version: 0.0.0.0"
         Me.Lbl_RaffyVersion.TextAlign = System.Drawing.ContentAlignment.TopCenter
@@ -85,17 +89,18 @@ Partial Class form_main
         Me.Lbl_UpdaterVersion.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Lbl_UpdaterVersion.AutoSize = True
         Me.Lbl_UpdaterVersion.Font = New System.Drawing.Font("Segoe UI", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Lbl_UpdaterVersion.Location = New System.Drawing.Point(532, 255)
+        Me.Lbl_UpdaterVersion.Location = New System.Drawing.Point(798, 382)
+        Me.Lbl_UpdaterVersion.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Lbl_UpdaterVersion.Name = "Lbl_UpdaterVersion"
-        Me.Lbl_UpdaterVersion.Size = New System.Drawing.Size(37, 12)
+        Me.Lbl_UpdaterVersion.Size = New System.Drawing.Size(61, 19)
         Me.Lbl_UpdaterVersion.TabIndex = 5
         Me.Lbl_UpdaterVersion.Text = "v 1.0.0.0"
         '
         'form_main
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(144.0!, 144.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
-        Me.ClientSize = New System.Drawing.Size(584, 276)
+        Me.ClientSize = New System.Drawing.Size(876, 414)
         Me.Controls.Add(Me.Lbl_UpdaterVersion)
         Me.Controls.Add(Me.Lbl_RaffyVersion)
         Me.Controls.Add(Me.Lbl_Status)
@@ -105,6 +110,7 @@ Partial Class form_main
         Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "form_main"
